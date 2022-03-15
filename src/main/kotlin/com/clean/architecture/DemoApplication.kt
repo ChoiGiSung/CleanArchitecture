df3da1f92a0872358de348fe153a3711ceb08afe
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-03-16 00:55:59 +0900

    feat : 패키지 이름 변경

--- src/main/kotlin/com/clean/architecture/DemoApplication.kt
+++ src/main/kotlin/com/clean/architecture/DemoApplication.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package com.clean.architecture
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/clean/architecture/DemoApplicationTests.kt
+++ src/test/kotlin/com/clean/architecture/DemoApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package com.clean.architecture
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
